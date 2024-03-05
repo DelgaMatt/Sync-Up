4d608a91d9b81b49e798e575d95cf84ee5bcab4b
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2024-03-04 19:50:40 -0800

    router clean up

--- lib/app/router/sync_router.dart
+++ lib/app/router/sync_router.dart
@@ -5,50 +5,52 @@ import 'package:sync_up/pages/home_page.dart';
 import 'package:sync_up/pages/splash_page.dart';
 
 enum RoutePath {
-  initial(path: '/auth'),
-  home(path: 'home'),
-  contact(path: 'contact'),
-  events(path: 'events'),
-  admin(path: 'admin'); //will need specific priveledges associated with user
+  initial(path: '/'),
+  home(path: '/home'),
+  contact(path: '/contact'),
+  events(path: '/events'),
+  splash(path: '/splash'),
+  admin(path: '/admin'); //will need specific priveledges associated with user
 
   const RoutePath({required this.path});
   final String path;
 }
 
-final GoRouter syncRouter =
-    GoRouter(initialLocation: RoutePath.initial.path, routes: <GoRoute>[
+final GoRouter syncRouter = GoRouter(
+  initialLocation: RoutePath.initial.path,
+  routes: _routes,
+  redirect: (BuildContext context, GoRouterState state) {
+    if (supabase.auth.currentSession != null) {
+      return RoutePath.home.path;
+    } else {
+      return null;
+    }
+  },
+);
+
+final _routes = [
   GoRoute(
-    redirect: (BuildContext context, GoRouterState state) {
-      if (supabase.auth.currentSession != null) {
-        return RoutePath.initial.path;
-      } else {
-        return null;
-      }
-    },
     path: RoutePath.initial.path,
-    pageBuilder: (BuildContext context, GoRouterState state) => MaterialPage(child: AuthPage()),
-    routes: <GoRoute>[
-      GoRoute(
-        path: RoutePath.home.path,
-        builder: (context, state) => const HomePage(),
-      ),
-      // GoRoute(
-      //   path: RoutePath.contact.path,
-      //   pageBuilder: (context, state) => const MaterialPage(child: ContactPage()),
-      //   ),
-      // GoRoute(
-      //   path: RoutePath.events.path,
-      //   pageBuilder: (context, state) => const MaterialPage(child: EventsPage()),
-      //   ),
-      // GoRoute(
-      //   path: RoutePath.admin.path,
-      //   pageBuilder: (context, state) => const MaterialPage(child: AdminPage()),
-      //   ),
-      GoRoute(
-        path: 'splash',
-        pageBuilder: (context, state) =>
-            const MaterialPage(child: SplashPage()),
-      ),
-    ],
+    pageBuilder: (context, state) => const MaterialPage(child: AuthPage()),
   ),
-]);
+  GoRoute(
+    path: RoutePath.home.path,
+    pageBuilder: (context, state) => const MaterialPage(child: HomePage()),
+  ),
+  // GoRoute(
+  //   path: RoutePath.contact.path,
+  //   pageBuilder: (context, state) => const MaterialPage(child: ContactPage()),
+  //   ),
+  // GoRoute(
+  //   path: RoutePath.events.path,
+  //   pageBuilder: (context, state) => const MaterialPage(child: EventsPage()),
+  //   ),
+  // GoRoute(
+  //   path: RoutePath.admin.path,
+  //   pageBuilder: (context, state) => const MaterialPage(child: AdminPage()),
+  //   ),
+  GoRoute(
+    path: RoutePath.splash.path,
+    pageBuilder: (context, state) => const MaterialPage(child: SplashPage()),
+  ),
+];
